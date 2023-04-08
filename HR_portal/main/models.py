c607e8aae76db957b576bc43a1a3c4f344eb64e9
KT-hue-pixel <nanalwelu1997@gmail.com>
2023-04-08 21:48:22 +0000

    created the view of the project

--- HR_portal/main/models.py
+++ HR_portal/main/models.py
@@ -4,11 +4,13 @@ from django.core.validators import RegexValidator
 
 # Create your models here.
 class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    phone = models.IntegerField(validators=[RegexValidator(r'^[0-9]*$', 'Only numbers are allowed.')])
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,10}$',  # accepts +<country code><phone number>
+        message="Phone number must be entered in the format: '+999999999'. Up to 10 digits allowed.",
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=10, blank=True, unique=True)  # accepts '+999999999'
     address = models.CharField(max_length=100)
     id_number = models.CharField(max_length=15, validators=[RegexValidator(r'^[a-zA-Z0-9]*$', 'Only alphanumeric characters are allowed.')]) 
     ssnit_number = models.CharField(max_length=13, validators=[RegexValidator(r'^[a-zA-Z0-9]*$', 'Only alphanumeric characters are allowed.')])
@@ -25,15 +27,22 @@ class Employee(models.Model):
     jobs = models.CharField(max_length= 3, choices=JOBS, default='WH')
     Guarantor = models.ForeignKey('Guarantor', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name + " " + self.jobs
+
 class Guarantor(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=100)
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,10}$',  # accepts +<country code><phone number>
+        message="Phone number must be entered in the format: '+999999999'. Up to 10 digits allowed.",
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=10, blank=True, unique=True)  # accepts '+999999999'
     address = models.CharField(max_length=100)
     relationship = models.CharField(max_length=100)
     occupation = models.CharField(max_length=100)
-    id_no= models.CharField(max_length=13, validators=[RegexValidator(r'^[a-zA-Z0-9]*$', 'Only alphanumeric characters are allowed.')])
+    id_no= models.CharField(max_length=15, validators=[RegexValidator(r'^[a-zA-Z0-9]*$', 'Only alphanumeric characters are allowed.')])
     
     def __str__(self):
-        return self.name + " " + self.phone
+        return self.name + " " + self.phone_number
     
